613de33d70bf047dfa1dec96cf9a9e3a23745699
Beda Tse <beda@coeus.hk>
2019-12-03 23:41:00 +0800

    added sonarqube

---
+++ main.tf
@@ -0,0 +1,29 @@
+resource "azurerm_resource_group" "sonarqube_aci" {
+  name     = "sonarqube-rg"
+  location = "eastasia"
+}
+
+resource "azurerm_container_group" "sonarqube" {
+  name                = "sonarqube-aci"
+  location            = "${azurerm_resource_group.sonarqube_aci.location}"
+  resource_group_name = "${azurerm_resource_group.sonarqube_aci.name}"
+  ip_address_type     = "public"
+  dns_name_label      = "coeus-aci"
+  os_type             = "Linux"
+
+  container {
+    name   = "Sonarqube 8.0 Community"
+    image  = "sonarqube:community-beta"
+    cpu    = "4"
+    memory = "8"
+
+    ports {
+      port     = 9000
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    environment = "sonarqube"
+  }
+}
